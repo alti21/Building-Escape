a38ff83f23c28ccbb45f6893507f8aa3719487e1
alti21 <altemush@gmail.com>
2019-03-24 15:28:14 -0700

    Finished basic layout of room, and output locaiton info to console

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AActor* Owner = GetOwner();//Find the owning actor
+
+	//Make a rotator
+	FRotator NewRotation = FRotator(0.0f,-60.0f,0.0f);//FRotator is a struct
+	//Set the door rotation
+	Owner->SetActorRotation(NewRotation);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
-
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
@@ -16,10 +16,14 @@ UPositionReport::UPositionReport()
 
 // Called when the game starts
 void UPositionReport::BeginPlay()
-{
+{//pointers tells which memory address to visit to find the actual value!!!
 	Super::BeginPlay();//do whatever happens up the inheritance tree
-
-	// ...
+	FString ObjectName = GetOwner()->GetName();//get name of object in game world, i.e. chair or rock
+	//use -> when GetOwner() is a pointer, use . when GetOwner is not a pointer
+	//FString ObjectPos = GetOwner()->GetActorLocation().ToString();//get x,y,z position of each object and convert to string
+	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();//alternative way for above comment
+	//NOTE: since GetOwner() has a * before it, use -> after it, GetTransform() has no *, use . operator
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos);//log, warning, or error
 	
 }
 
