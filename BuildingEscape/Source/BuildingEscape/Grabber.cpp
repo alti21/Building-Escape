e9d06113ac576d8a84ed405b98555c28088ce1ba
alti21 <altemush@gmail.com>
2019-03-29 21:18:24 -0700

    Added physics to table and chair, and now player can grab, move, and release the chair and table

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -19,11 +19,54 @@ UGrabber::UGrabber()
 // Called when the game starts
 void UGrabber::BeginPlay()
 {
-	Super::BeginPlay();
+	Super::BeginPlay();//goes up the heirarchy, goes to class UActorComponent
+	FindPhysicsHandleComponent();
+	//UE_LOG(LogTemp, Warning, TEXT("grabber reporting"));
+	SetupInputComponent();
 
-	UE_LOG(LogTemp, Warning, TEXT("grabber reporting"));
-	
 }
+
+// Look for attached Physics handle
+void UGrabber::FindPhysicsHandleComponent()
+{
+	
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	//GetOwner() get's the owner and FindComponentByClass looks down the owner (i.e., the default pawn/player components)
+	//and looks for the component of type UphysicsHandleComponent, which is the PhysicsHandle
+	if (PhysicsHandle)
+	{
+		//Physics handle is found
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s missing physics handle component"), *(GetOwner()->GetName()))
+	}
+}
+
+// Look for attached input component, only appears at run time
+void UGrabber::SetupInputComponent()
+{
+	
+	InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+	//GetOwner() get's the owner and FindComponentByClass looks down the owner (i.e., the default pawn/player components)
+	//and looks for the component of type UInputComponent, which is the InputComponent, InputComponent only appears at runtime, when game is played
+	if (InputComponent)
+	{
+		//InputComponent is found
+		UE_LOG(LogTemp, Warning, TEXT("Input component found"))
+			// Bind the input axis, this pointer is a reference to the grabber component itself
+			InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+		//here we are binding any key that maps to "Grab" (under project settings/input/action mappings), when key is pressed, we look at "this", which is 
+		//the Grabber then we look for a method called Grab, Grab function is called when we push the key(s)
+		InputComponent->BindAction("Grab", IE_Released, this, &UGrabber::Release);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s missing input component"), *(GetOwner()->GetName()))
+	}
+}
+
+
 
 
 // Called every frame
@@ -35,29 +78,80 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	FVector PlayerViewPointLocation;
 	FRotator PlayerViewPointRotation;
 	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(//OUT is a macro
-		OUT PlayerViewPointLocation, 
+		OUT PlayerViewPointLocation,
 		OUT PlayerViewPointRotation
 	);//for now, OUT does nothing. 
+
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;//use this for reaching and grabbing chair?
+
+	//if the physics handle is attached
+	if (PhysicsHandle->GrabbedComponent)
+	{//move object that we are holding
+		PhysicsHandle->SetTargetLocation(LineTraceEnd);
+	}
+		
+
 	
+}
+
+void UGrabber::Grab()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Grab pressed"))
+
+	//Line trace and see if we reach any actors with physics body collision set
+	auto HitResult = GetFirstPhysicsBodyInReach();
+	auto ComponentToGrab = HitResult.GetComponent();//GetComponent is of type UPrimitiveComponent, which is what we need in PhysicsHandle->GrabComponent(...)
+	auto ActorHit = HitResult.GetActor();
+
+	//if we hit something attach a physics handle
+	if (ActorHit)
+	{
+		// TODO attach physics handle
+		PhysicsHandle->GrabComponent(
+			ComponentToGrab,
+			NAME_None,
+			ComponentToGrab->GetOwner()->GetActorLocation(),
+			true
+		);
+	}
+
+}
+
+void UGrabber::Release()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Grab released"))
+		PhysicsHandle->ReleaseComponent();
+}
+
+const FHitResult UGrabber::GetFirstPhysicsBodyInReach()
+{
+	// Get Player view point this tick
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(//OUT is a macro
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotation
+	);//for now, OUT does nothing. 
+
 	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;//use this for reaching and grabbing chair?
 	// Draw a red trace in the world to visualize grabber reach
-	DrawDebugLine(
-		GetWorld(),
-		PlayerViewPointLocation, //line start
-		LineTraceEnd,//endpoint will be 20 cm above player's head
-		FColor(255, 0, 0),//color of line is red
-		false,//don't want line to persist
-		0.0f,//lifetime is not relevant since line is not persisting
-		0.0f,
-		10.0f
-	);
+	//DrawDebugLine(
+	//	GetWorld(),
+	//	PlayerViewPointLocation, //line start
+	//	LineTraceEnd,//endpoint will be 20 cm above player's head
+	//	FColor(255, 0, 0),//color of line is red
+	//	false,//don't want line to persist
+	//	0.0f,//lifetime is not relevant since line is not persisting
+	//	0.0f,
+	//	10.0f
+	//);
 	// Setup query parameters
 	FCollisionQueryParams TraceParameters(
-		FName(TEXT("")), 
+		FName(TEXT("")),
 		false, //we want to do a simple trace into the simple colliders, not a complex trace, true if you want to, i.e. collide with arms of chair
 		GetOwner()// we want to ignore ourselves because the ray starts from the center of the sphere(defaultpawn, ourself) so we don't want the first 
 		//hit to be ourself
-		);
+	);
 	// Line trace (ray-cast) out to reach distance, objectType because physicsBody is the objectType
 	FHitResult Hit;
 	GetWorld()->LineTraceSingleByObjectType(
@@ -67,12 +161,15 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),//use :: to access enum members
 		TraceParameters
 	);
+
+	// See what we hit
 	AActor* ActorHit = Hit.GetActor();
 	if (ActorHit)
 	{
 		UE_LOG(LogTemp, Warning, TEXT("Line trace hit %s"), *(ActorHit->GetName()));
 	}//when moving around, if you hit the table or chair, this will be written to console
-		
-	// See what we hit
+
+
+	return Hit;
 }
 
--- BuildingEscape/Source/BuildingEscape/Grabber.h
+++ BuildingEscape/Source/BuildingEscape/Grabber.h
@@ -4,6 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Components/InputComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "Classes/Components/PrimitiveComponent.h"
 #include "DrawDebugHelpers.h"
 #include "Grabber.generated.h"
 
@@ -28,4 +31,22 @@ public:
 private:
 	float Reach = 100.0f;
 	
+	UPhysicsHandleComponent* PhysicsHandle = nullptr;
+
+	UInputComponent* InputComponent = nullptr;
+
+	//Ray-cast and grab what's in reach
+	void Grab();
+
+	//Called when grab is reelased
+	void Release();
+
+	//Find attached physics handle
+	void FindPhysicsHandleComponent();
+
+	//Setup (assumed) attached input component
+	void SetupInputComponent();
+
+	//Return hit for first physics body in reach
+	const FHitResult GetFirstPhysicsBodyInReach();
 };
